6eefa665e022661f0a3fff738206b98fec43b51d
rerender2021 <rerender2021@outlook.com>
2022-03-26 09:05:12 +0800

    add dev docs

--- sidebars.js
+++ sidebars.js
@@ -8,7 +8,7 @@
  */
 
 module.exports = {
-    docs: ['introduction', 'getting-started'],
+    docs: ['introduction', 'getting-started', 'development'],
     components: [
         'components',
         'window',
